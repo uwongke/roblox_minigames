05e8507ee986c4f938ab71eb661dad60c6f6ec3b
Aaron <ajadriano2000@gmail.com>
2023-04-10 02:58:49 -0700

    Revert "Revert "New packages and minigame stuff""
    This reverts commit 805ecb077e9887f63a8e26d651e5812521b8e08a.

---
+++ src/Server/CmdrCommands/goToPlace.lua
@@ -0,0 +1,22 @@
+return {
+	Name = "goto-place";
+	Aliases = {};
+	Description = "Teleport to a Roblox place";
+	Group = "Admin";
+	AutoExec = {
+		"alias \"follow-player|Join a player in another server\" goto-place $1{players|Players} ${{get-player-place-instance $2{playerId|Target}}}",
+		"alias \"rejoin|Rejoin this place. You might end up in a different server.\" goto-place $1{players|Players} ${get-player-place-instance ${me} PlaceId}"
+	};
+	Args = {
+		{
+			Type = "players";
+			Name = "Players";
+			Description = "The players you want to teleport";
+		},
+		{
+			Type = "place";
+			Name = "Universe Place";
+			Description = "The alias of the in-universe place you want to teleport to";
+		}
+	};
+}
