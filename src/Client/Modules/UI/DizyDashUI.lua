b515272246d1dc015a9a0432f46d1531875682ca
scottman2691 <scottbgilman@gmail.com>
2023-04-12 15:02:55 -0400

    Update DizyDashUI.lua

--- src/Client/Modules/UI/DizyDashUI.lua
+++ src/Client/Modules/UI/DizyDashUI.lua
@@ -1,6 +1,7 @@
 local module = {}
 module.__index = module
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 local HttpsService = game:GetService("HttpService")
 local Template = ReplicatedStorage.Assets.UI.MiniGames.DefaultUI
 local Player = game:GetService("Players").LocalPlayer
@@ -9,6 +10,8 @@ local playerScripts = Player:WaitForChild("PlayerScripts")
 local playerModule = require(playerScripts:WaitForChild("PlayerModule"))
 local movementController = playerModule:GetControls()
 module.ID = script.Name
+local cam = workspace.CurrentCamera
+local offset = Vector3.new(0,1,3)
 
 function module.new(controller)
     local ui = {}
@@ -19,6 +22,7 @@ function module.new(controller)
     ui.TitleBar = ui.GUI.Frame.TitleBar
     ui.Direction = 1
     setmetatable(ui,module)
+    ui:DisableMovement()
     return ui
 end
 
@@ -32,7 +36,7 @@ function module:HandleMessage(message)
             if textField then
                 textField.Visible = val ~= ""
                 if val == "Go!" then
-                    self:DisableMovement()
+                    --self:DisableMovement()
                 else
                     if string.find(val, "Inverse") and textField.Text ~= val then
                         self.Direction *= -1
@@ -52,13 +56,26 @@ end
 
 -- Disables movement.
 function module:DisableMovement()
+    cam.CameraType = Enum.CameraType.Scriptable
+    self.tick = RunService.Stepped:Connect(function(time, deltaTime)
+        module:UpdateCamera()
+    end)
     movementController.moveFunction = function(player, direction, relative)
         Player.Move(player, direction * self.Direction, relative)
     end
 end
 
+function module:UpdateCamera()
+    cam.CFrame = CFrame.new(Player.Character.PrimaryPart.CFrame.Position + offset * 5) * CFrame.new(-offset)
+end
+
 -- Unbinds our "disable movement" so that the default keybinds are activated.
 function module:EnableMovement()
+    if self.tick then
+        self.tick:Disconnect()
+        self.tick = nil
+    end
+    cam.CameraType = Enum.CameraType.Custom
     movementController.moveFunction = Player.Move
 end
 
