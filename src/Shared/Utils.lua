5d532033b5ca9cb2e7bc8f88651c4ec94963b7c6
Aaron <ajadriano2000@gmail.com>
2023-04-25 13:19:41 -0700

    Update Utils.lua

--- src/Shared/Utils.lua
+++ src/Shared/Utils.lua
@@ -235,4 +235,17 @@ function Utils.calculateCustomVelocity(origin, direction,
     return customVelocity
 end
 
+function Utils.SecondsToClock(seconds)
+    local seconds = tonumber(seconds)
+  
+    if seconds <= 0 then
+      return "00:00";
+    else
+      local hours = string.format("%02.f", math.floor(seconds/3600));
+      local mins = string.format("%02.f", math.floor(seconds/60 - (hours*60)));
+      local secs = string.format("%02.f", math.floor(seconds - hours*3600 - mins *60));
+      return mins..":"..secs
+    end
+  end
+
 return Utils
